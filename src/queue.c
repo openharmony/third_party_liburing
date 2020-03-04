openharmony/third_party_liburing

commit cfed944839b4558d39810ca0d81983a7ab228c05

__io_uring_get_cqe: fix spurious -EAGAIN
A previous patch always cleared the number to wait for once we'd done
on io_uring_enter(), but we need to be sure that we've actually waited
for the right amount before doing so. Check this by matching the
return value with what we asked to submit - if it matches, we know we
can safely clear 'wait_nr'.

We also need to ensure that the original wait_nr is retained for the
peek test.

Fixes: 449cb211ed07 ("__io_uring_get_cqe: eliminate unnecessary io_uring_enter() syscalls")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/queue.c
@@ -35,6 +35,7 @@ int __io_uring_get_cqe(struct io_uring *ring, struct io_uring_cqe **cqe_ptr,
 		       unsigned submit, unsigned wait_nr, sigset_t *sigmask)
 {
 	struct io_uring_cqe *cqe = NULL;
+	const int to_wait = wait_nr;
 	int ret = 0, err;
 
 	do {
@@ -43,7 +44,7 @@ int __io_uring_get_cqe(struct io_uring *ring, struct io_uring_cqe **cqe_ptr,
 		err = __io_uring_peek_cqe(ring, &cqe);
 		if (err)
 			break;
-		if (!cqe && !wait_nr && !submit) {
+		if (!cqe && !to_wait && !submit) {
 			err = -EAGAIN;
 			break;
 		}
@@ -56,10 +57,14 @@ int __io_uring_get_cqe(struct io_uring *ring, struct io_uring_cqe **cqe_ptr,
 						   wait_nr, flags, sigmask);
 		if (wait_nr)
 			wait_nr = 0;
-		if (ret < 0)
+		if (ret < 0) {
 			err = -errno;
-		else
+		} else if (ret == submit) {
+			submit = 0;
+			wait_nr = 0;
+		} else {
 			submit -= ret;
+		}
 		if (cqe)
 			break;
 	} while (!err);

commit d2bdbea8735274d1399ea53514a4156a7ffc251a

__io_uring_get_cqe: remove redundant wait_nr clear
Fixes: cfed944839b4 ("__io_uring_get_cqe: fix spurious -EAGAIN")
Reported-by: Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/queue.c
@@ -55,8 +55,6 @@ int __io_uring_get_cqe(struct io_uring *ring, struct io_uring_cqe **cqe_ptr,
 		if (wait_nr || submit)
 			ret = __sys_io_uring_enter(ring->ring_fd, submit,
 						   wait_nr, flags, sigmask);
-		if (wait_nr)
-			wait_nr = 0;
 		if (ret < 0) {
 			err = -errno;
 		} else if (ret == submit) {