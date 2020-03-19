axboe/liburing

commit 783831a0548d3c702bd18746b5f425106f10e090

Include MIT notice in rpm spec and debian package file
Signed-off-by: Jens Axboe <axboe@kernel.dk>

liburing.spec
@@ -2,7 +2,7 @@ Name: liburing
 Version: 0.3
 Release: 1%{?dist}
 Summary: Linux-native io_uring I/O access library
-License: LGPLv2+
+License: LGPLv2+ / MIT
 Source: https://git.kernel.dk/cgit/liburing/snapshot/%{name}-%{version}.tar.gz
 URL: https://git.kernel.dk/cgit/liburing/
 BuildRequires: gcc

debian/copyright
@@ -4,7 +4,7 @@ Source: https://git.kernel.dk/cgit/liburing/
 
 Files: *
 Copyright: 2019 Jens Axboe <axboe@kernel.dk>
-License: GPL-2+
+License: GPL-2+ / MIT
 
 Files: debian/*
 Copyright: 2019 Changcheng Liu <changcheng.liu@aliyun.com>
@@ -26,3 +26,24 @@ License: GPL-2+
  .
  On Debian systems, the complete text of the GNU General
  Public License version 2 can be found in "/usr/share/common-licenses/GPL-2".
+
+License: MIT
+ Copyright 2020 Jens Axboe
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.

openharmony/third_party_liburing

commit 522be3a703bc15e637a4fcfb624432de062cf70d

spec: use "or" instead of "/" in License line
rpmlint on Fedora 31 complains about "LGPLv2+ / MIT" because "/" is not
allowed.  Use "or" instead.

Fixes: 783831a0548d3c702bd18746b5f425106f10e090
       ("Include MIT notice in rpm spec and debian package file")
Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
Signed-off-by: Jens Axboe <axboe@kernel.dk>

liburing.spec
@@ -2,7 +2,7 @@ Name: liburing
 Version: 0.6
 Release: 1%{?dist}
 Summary: Linux-native io_uring I/O access library
-License: LGPLv2+ / MIT
+License: LGPLv2+ or MIT
 Source0: https://brick.kernel.dk/snaps/%{name}-%{version}.tar.gz
 Source1: https://brick.kernel.dk/snaps/%{name}-%{version}.tar.gz.asc
 URL: https://git.kernel.dk/cgit/liburing/