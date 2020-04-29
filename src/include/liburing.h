openharmony/third_party_liburing

commit 58e2a2113be3392edf72e95271875e967dc095aa

Use __off64_t for offsets
loff_t isn't available everywhere, and off_t is only 32-bits on some
archs.

Reported-by: Milan P. Stanić <mps@arvanta.net>
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/include/liburing.h
@@ -193,8 +193,8 @@ static inline void io_uring_prep_rw(int op, struct io_uring_sqe *sqe, int fd,
 }
 
 static inline void io_uring_prep_splice(struct io_uring_sqe *sqe,
-					int fd_in, loff_t off_in,
-					int fd_out, loff_t off_out,
+					int fd_in, __off64_t off_in,
+					int fd_out, __off64_t off_out,
 					unsigned int nbytes,
 					unsigned int splice_flags)
 {

commit 70ed32af246ddc90abc4dcccbe920a1ef52fb04a

Use uint64_t for splice offsets
Apparently __off64_t is a glibc'ism. Give up on the offset types, let's
just use a 64-bit unsigned type.

Fixes: 58e2a2113be3 ("Use __off64_t for offsets")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/include/liburing.h
@@ -193,8 +193,8 @@ static inline void io_uring_prep_rw(int op, struct io_uring_sqe *sqe, int fd,
 }
 
 static inline void io_uring_prep_splice(struct io_uring_sqe *sqe,
-					int fd_in, __off64_t off_in,
-					int fd_out, __off64_t off_out,
+					int fd_in, uint64_t off_in,
+					int fd_out, uint64_t off_out,
 					unsigned int nbytes,
 					unsigned int splice_flags)
 {