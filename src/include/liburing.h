openharmony/third_party_liburing

commit c7bf02a38da99fdab31c21d40da22b9c3adaccd1

Move io_uring_get_sqe() inline in the liburing.h header
It's a fast path function, we should not be making it a function call
into the library.

Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/include/liburing.h
@@ -127,7 +127,6 @@ int io_uring_submit_and_wait_timeout(struct io_uring *ring,
 				     unsigned wait_nr,
 				     struct __kernel_timespec *ts,
 				     sigset_t *sigmask);
-struct io_uring_sqe *io_uring_get_sqe(struct io_uring *ring);
 
 int io_uring_register_buffers(struct io_uring *ring, const struct iovec *iovecs,
 			      unsigned nr_iovecs);
@@ -823,6 +822,27 @@ static inline int io_uring_wait_cqe(struct io_uring *ring,
 				    struct io_uring_cqe **cqe_ptr)
 {
 	return io_uring_wait_cqe_nr(ring, cqe_ptr, 1);
+}
+
+/*
+ * Return an sqe to fill. Application must later call io_uring_submit()
+ * when it's ready to tell the kernel about it. The caller may call this
+ * function multiple times before calling io_uring_submit().
+ *
+ * Returns a vacant sqe, or NULL if we're full.
+ */
+static inline struct io_uring_sqe *io_uring_get_sqe(struct io_uring *ring)
+{
+	struct io_uring_sq *sq = &ring->sq;
+	unsigned int head = io_uring_smp_load_acquire(sq->khead);
+	unsigned int next = sq->sqe_tail + 1;
+	struct io_uring_sqe *sqe = NULL;
+
+	if (next - head <= *sq->kring_entries) {
+		sqe = &sq->sqes[sq->sqe_tail & *sq->kring_mask];
+		sq->sqe_tail = next;
+	}
+	return sqe;
 }
 
 ssize_t io_uring_mlock_size(unsigned entries, unsigned flags);

src/queue.c
@@ -403,27 +403,6 @@ int io_uring_submit_and_wait(struct io_uring *ring, unsigned wait_nr)
 	return __io_uring_submit_and_wait(ring, wait_nr);
 }
 
-/*
- * Return an sqe to fill. Application must later call io_uring_submit()
- * when it's ready to tell the kernel about it. The caller may call this
- * function multiple times before calling io_uring_submit().
- *
- * Returns a vacant sqe, or NULL if we're full.
- */
-struct io_uring_sqe *io_uring_get_sqe(struct io_uring *ring)
-{
-	struct io_uring_sq *sq = &ring->sq;
-	unsigned int head = io_uring_smp_load_acquire(sq->khead);
-	unsigned int next = sq->sqe_tail + 1;
-	struct io_uring_sqe *sqe = NULL;
-
-	if (next - head <= *sq->kring_entries) {
-		sqe = &sq->sqes[sq->sqe_tail & *sq->kring_mask];
-		sq->sqe_tail = next;
-	}
-	return sqe;
-}
-
 int __io_uring_sqring_wait(struct io_uring *ring)
 {
 	return  ____sys_io_uring_enter(ring->ring_fd, 0, 0,

commit 74212455ee0a48ed6be2f22482c8d053ac021a68

queue: provide io_uring_get_sqe() symbol again
With a recent change, io_uring_get_sqe() was moved to the header file
to avoid a function call for this hot function. However, this will break
updates of the library, where the symbol is then not provided:

./app: symbol lookup error: ./app: undefined symbol: io_uring_get_sqe, version LIBURING_2.0

Provide one internally so we still export it, but allow newly compiled
applications to grab the one from the header. That should provide the
best of both worlds.

Fixes: c7bf02a38da9 ("Move io_uring_get_sqe() inline in the liburing.h header")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/Makefile
@@ -9,7 +9,7 @@ CPPFLAGS ?=
 override CPPFLAGS += -D_GNU_SOURCE \
 	-Iinclude/ -include ../config-host.h
 CFLAGS ?= -g -fomit-frame-pointer -O2 -Wall -Wextra
-override CFLAGS += -Wno-unused-parameter -Wno-sign-compare
+override CFLAGS += -Wno-unused-parameter -Wno-sign-compare -DLIBURING_INTERNAL
 SO_CFLAGS=-fPIC $(CFLAGS)
 L_CFLAGS=$(CFLAGS)
 LINK_FLAGS=

src/include/liburing.h
@@ -831,6 +831,7 @@ static inline int io_uring_wait_cqe(struct io_uring *ring,
  *
  * Returns a vacant sqe, or NULL if we're full.
  */
+#ifndef LIBURING_INTERNAL
 static inline struct io_uring_sqe *io_uring_get_sqe(struct io_uring *ring)
 {
 	struct io_uring_sq *sq = &ring->sq;
@@ -844,6 +845,9 @@ static inline struct io_uring_sqe *io_uring_get_sqe(struct io_uring *ring)
 	}
 	return sqe;
 }
+#else
+struct io_uring_sqe *io_uring_get_sqe(struct io_uring *ring);
+#endif
 
 ssize_t io_uring_mlock_size(unsigned entries, unsigned flags);
 ssize_t io_uring_mlock_size_params(unsigned entries, struct io_uring_params *p);

src/queue.c
@@ -403,6 +403,20 @@ int io_uring_submit_and_wait(struct io_uring *ring, unsigned wait_nr)
 	return __io_uring_submit_and_wait(ring, wait_nr);
 }
 
+struct io_uring_sqe *io_uring_get_sqe(struct io_uring *ring)
+{
+	struct io_uring_sq *sq = &ring->sq;
+	unsigned int head = io_uring_smp_load_acquire(sq->khead);
+	unsigned int next = sq->sqe_tail + 1;
+	struct io_uring_sqe *sqe = NULL;
+
+	if (next - head <= *sq->kring_entries) {
+		sqe = &sq->sqes[sq->sqe_tail & *sq->kring_mask];
+		sq->sqe_tail = next;
+	}
+	return sqe;
+}
+
 int __io_uring_sqring_wait(struct io_uring *ring)
 {
 	return  ____sys_io_uring_enter(ring->ring_fd, 0, 0,