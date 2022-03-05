openharmony/third_party_liburing

commit 302b1e7e8bce24cc6cb73fed8a2128ce7e1005eb

Fix __io_uring_get_cqe() for IORING_SETUP_IOPOLL
If __io_uring_get_cqe() is called for the ring setup with IOPOLL, we must enter the kernel
to get completion events. Even if that is called with wait_nr is zero.

Signed-off-by: Akinobu Mita <akinobu.mita@gmail.com>
Link: https://lore.kernel.org/r/20220210152924.14413-1-akinobu.mita@gmail.com
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/queue.c
@@ -31,6 +31,11 @@ static inline bool cq_ring_needs_flush(struct io_uring *ring)
 	return IO_URING_READ_ONCE(*ring->sq.kflags) & IORING_SQ_CQ_OVERFLOW;
 }
 
+static inline bool cq_ring_needs_enter(struct io_uring *ring)
+{
+	return (ring->flags & IORING_SETUP_IOPOLL) || cq_ring_needs_flush(ring);
+}
+
 static int __io_uring_peek_cqe(struct io_uring *ring,
 			       struct io_uring_cqe **cqe_ptr,
 			       unsigned *nr_available)
@@ -84,7 +89,6 @@ static int _io_uring_get_cqe(struct io_uring *ring, struct io_uring_cqe **cqe_pt
 
 	do {
 		bool need_enter = false;
-		bool cq_overflow_flush = false;
 		unsigned flags = 0;
 		unsigned nr_available;
 		int ret;
@@ -93,13 +97,13 @@ static int _io_uring_get_cqe(struct io_uring *ring, struct io_uring_cqe **cqe_pt
 		if (err)
 			break;
 		if (!cqe && !data->wait_nr && !data->submit) {
-			if (!cq_ring_needs_flush(ring)) {
+			if (!cq_ring_needs_enter(ring)) {
 				err = -EAGAIN;
 				break;
 			}
-			cq_overflow_flush = true;
+			need_enter = true;
 		}
-		if (data->wait_nr > nr_available || cq_overflow_flush) {
+		if (data->wait_nr > nr_available || need_enter) {
 			flags = IORING_ENTER_GETEVENTS | data->get_flags;
 			need_enter = true;
 		}

commit f92d4a94c7ec56bebbe6a11b1ec691892405fbfe

src/queue: don't keep looping for IOPOLL peek operation
A previous commit broke peek with an IOPOLL ring, making it loop
continually even if we have wait_for == 0 and submit == 0.

It's only necessary to enter the kernel for that condition once, don't
keep looping trying to poll for zero events.

Fixes: 302b1e7e8bce ("Fix __io_uring_get_cqe() for IORING_SETUP_IOPOLL")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/queue.c
@@ -85,6 +85,7 @@ static int _io_uring_get_cqe(struct io_uring *ring, struct io_uring_cqe **cqe_pt
 			     struct get_data *data)
 {
 	struct io_uring_cqe *cqe = NULL;
+	bool looped = false;
 	int err;
 
 	do {
@@ -97,7 +98,12 @@ static int _io_uring_get_cqe(struct io_uring *ring, struct io_uring_cqe **cqe_pt
 		if (err)
 			break;
 		if (!cqe && !data->wait_nr && !data->submit) {
-			if (!cq_ring_needs_enter(ring)) {
+			/*
+			 * If we already looped once, we already entererd
+			 * the kernel. Since there's nothing to submit or
+			 * wait for, don't keep retrying.
+			 */
+			if (looped || !cq_ring_needs_enter(ring)) {
 				err = -EAGAIN;
 				break;
 			}
@@ -123,6 +129,7 @@ static int _io_uring_get_cqe(struct io_uring *ring, struct io_uring_cqe **cqe_pt
 		data->submit -= ret;
 		if (cqe)
 			break;
+		looped = true;
 	} while (1);
 
 	*cqe_ptr = cqe;