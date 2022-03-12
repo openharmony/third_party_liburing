openharmony/third_party_liburing

commit 0d27fb931f1c36fe94b518f10c0d21e201400d02

Make the fast path of CQE checking inline
Even the io_uring_peek_cqe() currently requires an indirect call into
the library, even if an event is readily available.

Inline the CQE check and retrieval for peek, and have io_uring_wait_cqe()
check that as well before calling into a library function.

Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/include/liburing.h
@@ -817,6 +817,44 @@ static inline int io_uring_wait_cqe_nr(struct io_uring *ring,
 	return __io_uring_get_cqe(ring, cqe_ptr, 0, wait_nr, NULL);
 }
 
+static inline int __io_uring_peek_cqe(struct io_uring *ring,
+				      struct io_uring_cqe **cqe_ptr,
+				      unsigned *nr_available)
+{
+	struct io_uring_cqe *cqe;
+	int err = 0;
+	unsigned available;
+	unsigned mask = *ring->cq.kring_mask;
+
+	do {
+		unsigned tail = io_uring_smp_load_acquire(ring->cq.ktail);
+		unsigned head = *ring->cq.khead;
+
+		cqe = NULL;
+		available = tail - head;
+		if (!available)
+			break;
+
+		cqe = &ring->cq.cqes[head & mask];
+		if (!(ring->features & IORING_FEAT_EXT_ARG) &&
+				cqe->user_data == LIBURING_UDATA_TIMEOUT) {
+			if (cqe->res < 0)
+				err = cqe->res;
+			io_uring_cq_advance(ring, 1);
+			if (!err)
+				continue;
+			cqe = NULL;
+		}
+
+		break;
+	} while (1);
+
+	*cqe_ptr = cqe;
+	if (nr_available)
+		*nr_available = available;
+	return err;
+}
+
 /*
  * Return an IO completion, if one is readily available. Returns 0 with
  * cqe_ptr filled in on success, -errno on failure.
@@ -824,6 +862,9 @@ static inline int io_uring_wait_cqe_nr(struct io_uring *ring,
 static inline int io_uring_peek_cqe(struct io_uring *ring,
 				    struct io_uring_cqe **cqe_ptr)
 {
+	if (__io_uring_peek_cqe(ring, cqe_ptr, NULL))
+		return 0;
+
 	return io_uring_wait_cqe_nr(ring, cqe_ptr, 0);
 }
 
@@ -834,6 +875,9 @@ static inline int io_uring_peek_cqe(struct io_uring *ring,
 static inline int io_uring_wait_cqe(struct io_uring *ring,
 				    struct io_uring_cqe **cqe_ptr)
 {
+	if (__io_uring_peek_cqe(ring, cqe_ptr, NULL))
+		return 0;
+
 	return io_uring_wait_cqe_nr(ring, cqe_ptr, 1);
 }
 

src/queue.c
@@ -43,43 +43,6 @@ static inline bool cq_ring_needs_enter(struct io_uring *ring)
 	return (ring->flags & IORING_SETUP_IOPOLL) || cq_ring_needs_flush(ring);
 }
 
-static int __io_uring_peek_cqe(struct io_uring *ring,
-			       struct io_uring_cqe **cqe_ptr,
-			       unsigned *nr_available)
-{
-	struct io_uring_cqe *cqe;
-	int err = 0;
-	unsigned available;
-	unsigned mask = *ring->cq.kring_mask;
-
-	do {
-		unsigned tail = io_uring_smp_load_acquire(ring->cq.ktail);
-		unsigned head = *ring->cq.khead;
-
-		cqe = NULL;
-		available = tail - head;
-		if (!available)
-			break;
-
-		cqe = &ring->cq.cqes[head & mask];
-		if (!(ring->features & IORING_FEAT_EXT_ARG) &&
-				cqe->user_data == LIBURING_UDATA_TIMEOUT) {
-			if (cqe->res < 0)
-				err = cqe->res;
-			io_uring_cq_advance(ring, 1);
-			if (!err)
-				continue;
-			cqe = NULL;
-		}
-
-		break;
-	} while (1);
-
-	*cqe_ptr = cqe;
-	*nr_available = available;
-	return err;
-}
-
 struct get_data {
 	unsigned submit;
 	unsigned wait_nr;

commit d9592e868c8f771d374a92e28363afdbde90328e

liburing: fix issue with fast-pathed peek
There are actually two bugs in that commit:

- Assuming that __io_uring_peek_cqe() returns 0 with cqe always filled,
  like we do for the library functions. It can return 0 and not fill
  in the cqe, caller should check cqe. The internals do that, but not
  the new additions.

- We were checking for non-zero return, we should check for 0.

Link: https://github.com/axboe/liburing/issues/546
Fixes: 0d27fb931f1c ("Make the fast path of CQE checking inline")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/include/liburing.h
@@ -862,7 +862,7 @@ static inline int __io_uring_peek_cqe(struct io_uring *ring,
 static inline int io_uring_peek_cqe(struct io_uring *ring,
 				    struct io_uring_cqe **cqe_ptr)
 {
-	if (__io_uring_peek_cqe(ring, cqe_ptr, NULL))
+	if (!__io_uring_peek_cqe(ring, cqe_ptr, NULL) && *cqe_ptr)
 		return 0;
 
 	return io_uring_wait_cqe_nr(ring, cqe_ptr, 0);
@@ -875,7 +875,7 @@ static inline int io_uring_peek_cqe(struct io_uring *ring,
 static inline int io_uring_wait_cqe(struct io_uring *ring,
 				    struct io_uring_cqe **cqe_ptr)
 {
-	if (__io_uring_peek_cqe(ring, cqe_ptr, NULL))
+	if (!__io_uring_peek_cqe(ring, cqe_ptr, NULL) && *cqe_ptr)
 		return 0;
 
 	return io_uring_wait_cqe_nr(ring, cqe_ptr, 1);