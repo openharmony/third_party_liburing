axboe/liburing

commit 4fb3c9e9c737c2cf2d4df4e1972e68d596a626f7

Add runtests-loop target
Signed-off-by: Jens Axboe <axboe@kernel.dk>

Makefile
@@ -15,6 +15,8 @@ all:
 
 runtests:
 	@$(MAKE) -C test runtests
+runtests-loop:
+	@$(MAKE) -C test runtests-loop
 
 config-host.mak: configure
 	@if [ ! -e "$@" ]; then					\

test/Makefile
@@ -37,3 +37,5 @@ clean:
 
 runtests:
 	@./runtests.sh $(all_targets)
+runtests-loop:
+	@./runtests-loop.sh $(all_targets)

test/runtests-loop.sh
@@ -1,0 +1,16 @@
+#!/bin/bash
+
+TESTS="$@"
+ITER=0
+
+while true; do
+	./runtests.sh "$TESTS"
+	RET="$?"
+	if [ "${RET}" -ne 0 ]; then
+		echo "Tests failed at loop $ITER"
+		break
+	fi
+	echo "Finished loop $ITER"
+	((ITER++))
+done
+

openharmony/third_party_liburing

commit 0c8d6fff77e06b17edf1dd4bfce8ea17b85d0ff0

Makefile: Make sure we build everything before runtests
A recent commit added `runtests-parallel` target to run the tests in
parallel. But it doesn't have a proper build dependency.

As such, when doing:

  make -j clean;
  make -j runtests-parallel;

we got this error:

```
  make[1]: Entering directory '/home/ammarfaizi2/app/liburing/test'
  make[1]: *** No rule to make target '232c93d07b74.t', needed by '232c93d07b74.run_test'.  Stop.
  make[1]: Leaving directory '/home/ammarfaizi2/app/liburing/test'
  make: *** [Makefile:25: runtests-parallel] Error 2
```

Add `all` target as the dependency of `runtests-parallel`. While in
there, I found the same issue on `runtests-loop` target. Do the same
thing for it too. This way the main Makefile will build everything
first before trying to execute the tests.

Cc: Dylan Yudaken <dylany@fb.com>
Fixes: 4fb3c9e9c737c2cf2d4df4e1972e68d596a626f7 ("Add runtests-loop target")
Fixes: a33e908d862d7748ac32ac36074860d026c62d8c ("test: add make targets for each test")
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>
Signed-off-by: Jens Axboe <axboe@kernel.dk>

Makefile
@@ -19,9 +19,9 @@ partcheck: all
 
 runtests: all
 	@$(MAKE) -C test runtests
-runtests-loop:
+runtests-loop: all
 	@$(MAKE) -C test runtests-loop
-runtests-parallel:
+runtests-parallel: all
 	@$(MAKE) -C test runtests-parallel
 
 config-host.mak: configure