openharmony/third_party_liburing

commit 94e3f34cba0ddb1c076e18aca466d96a8706589c

queue: avoid io_uring_enter() with no SQEs
io_uring_submit() makes an io_uring_enter() syscall even if no SQEs are
ready to submit. Move the empty SQ check from _io_uring_get_cqe() to
sq_ring_needs_enter() so io_uring_submit() can skip the syscall too.

Signed-off-by: Caleb Sander <csander@purestorage.com>

src/queue.c
@@ -12,9 +12,15 @@
  * Returns true if we're not using SQ thread (thus nobody submits but us)
  * or if IORING_SQ_NEED_WAKEUP is set, so submit thread must be explicitly
  * awakened. For the latter case, we set the thread wakeup flag.
+ * If no SQEs are ready for submission, returns false.
  */
-static inline bool sq_ring_needs_enter(struct io_uring *ring, unsigned *flags)
+static inline bool sq_ring_needs_enter(struct io_uring *ring,
+				       unsigned submit,
+				       unsigned *flags)
 {
+	if (!submit)
+		return false;
+
 	if (!(ring->flags & IORING_SETUP_SQPOLL))
 		return true;
 
@@ -85,7 +91,7 @@ static int _io_uring_get_cqe(struct io_uring *ring,
 			flags = IORING_ENTER_GETEVENTS | data->get_flags;
 			need_enter = true;
 		}
-		if (data->submit && sq_ring_needs_enter(ring, &flags))
+		if (sq_ring_needs_enter(ring, data->submit, &flags))
 			need_enter = true;
 		if (!need_enter)
 			break;
@@ -354,7 +360,7 @@ static int __io_uring_submit(struct io_uring *ring, unsigned submitted,
 	int ret;
 
 	flags = 0;
-	if (sq_ring_needs_enter(ring, &flags) || wait_nr) {
+	if (sq_ring_needs_enter(ring, submitted, &flags) || wait_nr) {
 		if (wait_nr || (ring->flags & IORING_SETUP_IOPOLL))
 			flags |= IORING_ENTER_GETEVENTS;
 		if (ring->int_flags & INT_FLAG_REG_RING)

commit 4308dd8a662e7e630010b62f7027907631c30607

src/queue: always enter for IOPOLL
The commit pinpointed in the fixes line re-introduced a regression
with IOPOLL. Reinstate the rule that IOPOLL should always enter
the kernel.

Fixes: 94e3f34cba0d ("queue: avoid io_uring_enter() with no SQEs")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/queue.c
@@ -18,6 +18,9 @@ static inline bool sq_ring_needs_enter(struct io_uring *ring,
 				       unsigned submit,
 				       unsigned *flags)
 {
+	/* see commit 1bafb3ce5f5e */
+	if (ring->flags & IORING_SETUP_IOPOLL)
+		return true;
 	if (!submit)
 		return false;
 